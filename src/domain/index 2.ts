35b7007dd113ee76d25317cbcff80c93cd7a4df3
tomohiro1850 <tmhr1850@gmail.com>
2025-06-23 20:38:43 +0900

    chore: 重複した不要ファイルを削除する (closes #21)

--- src/domain/index 2.ts
+++
@@ -1,7 +0,0 @@
-// Models
-export type { IPiece, IBoard } from './models/piece/interface';
-export { Player, PieceType } from './models/piece/types';
-export type { Position, Move } from './models/piece/types';
-
-// Services
-export { GameRules } from './services/game-rules';
